843df5a2752d97633c70ce26942efb095d8293be
natecheney-dev <natecheney@gmail.com>
2021-12-07 14:23:48 -0600

    finished router & added error handler to middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -78,9 +78,17 @@ const checkVinNumberUnique = async (req, res, next) => {
   }
 }
 
+function errorHandling(err, req, res, next) { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
 module.exports = {
   checkCarId, 
   checkCarPayload, 
   checkVinNumberValid, 
-  checkVinNumberUnique
+  checkVinNumberUnique,
+  errorHandling
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,4 +1,28 @@
 const router = require('express').Router()
 const Cars = require('./cars-model')
-const {checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid} = require('./cars-middleware')
+const {checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid, errorHandling} = require('./cars-middleware')
 
+router.get('/', (req, res, next) => {
+    Cars.getAll(req.params.id)
+        .then(cars => {
+            res.json(cars)
+        })
+        .catch(next)
+})
+
+router.get('/:id', checkCarId, (req, res) => {
+    res.json(req.db_car)
+})
+
+router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, async (req, res, next) => {
+    try {
+        const newCar = await Cars.create(req.body)
+        res.status(201).json(newCar)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.use(errorHandling);
+
+module.exports = router 
